86e4ce161a99e134233e66fe4aa6e9c90d67a1d4
nimba005 <nimbaoyugi@gmail.com>
2024-01-02 17:53:07 +0000

    all done

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Return information about his/her TODO list progress"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    user_id = argv[1]
+
+    url = "https://jsonplaceholder.typicode.com/"
+    uri_user_id = "users/{}".format(user_id)
+    uri_todos = "todos"
+    
+    user = requests.get(url + uri_user_id).json()
+    tasks = requests.get(url + uri_todos, params={"userId": user_id}).json()
+    cmt_tasks = [task for task in tasks if task.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
+                                                          len(cmt_tasks),
+                                                          len(tasks)))
+    [print("\t {}".format(task.get("title"))) for task in cmt_tasks]
